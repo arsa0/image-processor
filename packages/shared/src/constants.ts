72ecc851ef349a2e84161dac0ede726f32a2d2fe
Arsa Fauzi <arsafauzi@gmail.com>
2026-06-12 01:16:33 +0900

    add storage adapter (using AWS-SDK and minio as client)

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,2 +1,6 @@
 export const ALLOWED_MIME_TYPE = ["image/jpeg", "image/png", "image/webp"];
 export const MAX_UPLOAD_SIZE = 20971520;
+
+export const S3_PRESIGNED_DOWNLOAD_URL_DEFAULT_EXPIRES_SECONDS = 900;
+export const S3_PRESIGNED_DOWNLOAD_MIN_EXPIRES_SECONDS = 60;
+export const S3_PRESIGNED_DOWNLOAD_MAX_EXPIRES_SECONDS = 60 * 60 * 24;
